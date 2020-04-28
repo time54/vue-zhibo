ed780319ade149edb5ff852ffe598690c9c301da
wangyifan <wangyifan@myhexin.com>
2020-04-28 22:19:51 +0800

    flvjs 实现直播

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # vue-zhibo
-Flv.js + OBS 实现直播
+
+## 1、安装 flv.js
+```
+npm install --save flv.js
+```
+## 2、引入 flv.js
+```
+import flvjs from 'flv.js/dist/flv.min'【亦可设置全局引用：https://blog.csdn.net/HelloMyCode/article/details/103241200】
+```
+
+## 3、 template 内新增 video 标签
+
+视频需要放到服务器上
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video id="myvideo" @click="click" controls = "true" height="500" width="auto"></video>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import flvjs from 'flv.js/dist/flv.min'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    if (flvjs.isSupported()) {
+      var videoElement = document.getElementById('myvideo')
+      var flvPlayer = flvjs.createPlayer({
+        type: 'mp4',
+        url: 'http://khtest.10jqka.com.cn/dev/taojinchao/TEST/movie.mp4'
+      })
+      flvPlayer.attachMediaElement(videoElement)
+      flvPlayer.load()
+    }
+  },
+  methods: {
+    click() {
+      if (this.playing) {
+        this.player.pause()
+        this.playing = false
+      } else {
+        this.player.play()
+        this.playing = true
+      }
+    }
+  }
+}
+</script>
